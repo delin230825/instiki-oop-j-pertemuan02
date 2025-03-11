9482aa5a81503dee4868681c80d07bef2e588ca7
delinyotia <delinyotia044@gmail.com>
2025-03-11 21:57:35 +0800

    pertemuan 2

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,24 @@
+package javaapplication2;
+
+/**
+ *
+ * @author delyotia
+ * tgl: 11 Maret 2025
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Delyotia";
+        String nim = "2301010487";
+        
+        System.out.println("pertemuan 2 java");
+        System.out.print("~~~~~~~~~");
+        System.out.println("Delyotia  : "+nama);
+        System.out.println ("Nim      : "+nim);
+        System.out.printf("Delyotia %s, saat ini sedang di bali",nama,nim);
+    }
+    
+}
